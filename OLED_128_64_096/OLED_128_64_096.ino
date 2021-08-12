bb344843047f3ea70eb2ddc6cb48aba9de9ec332
Runnan Li <runnanl@blackmagicdesign.com>
2021-08-13 09:25:13 +1000

    Initial commit

---
+++ OLED_128_64_096/OLED_128_64_096.ino
@@ -0,0 +1,100 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+ 
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+const int TEMP = 2;
+const int RPM = 3;
+const int MHZ = 4;
+const int GHZ = 5;
+const int PERPCENT = 6;
+
+const char TEMP_c[3] = " C";
+const char RPM_c[4] = "rpm";
+const char MHZ_c[4] = "MHz";
+const char GHZ_c[4] = "GHz";
+const char PERCENT_c[2] = "%";
+
+const int MAX_W = 128;
+const int MAX_H = 64;
+const int PAGE_HEAD_PIXEL_H = 8;  // heading pixel height in page mode 
+const int PAGE_VAL_PIXEL_H = 20;  // value pixel height in page mode
+const int PAGE_VAL_PIXEL_W = 16;  // my estimation of pixel width in page mode
+ 
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);  // High speed I2C
+ 
+// U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    //Low spped I2C
+ 
+void setup(void) {
+//  Serial.begin(9600);
+  u8g2.begin();
+}
+
+int draw_page_contents(char heading[], char value_n_postfix[], int val_x_ofst, int val_y_ofst) {
+  u8g2.setFont(u8g2_font_ncenB08_tr);   // choose a suitable font
+  u8g2.drawStr(0,10,heading);    // write something to the internal memory
+  u8g2.setFont(u8g2_font_fub20_tr);
+  u8g2.drawStr(val_x_ofst,val_y_ofst,value_n_postfix);
+  return 0;
+}
+
+int find_x_ofst(char value[], char postfx_char[]) {
+  int len_val = strlen(value) - 1;
+  int len_pfx = strlen(postfx_char);
+//  Serial.println(len_val);
+//  Serial.println(len_pfx);
+  int x_start_pixel = (MAX_W - (len_val + len_pfx) * PAGE_VAL_PIXEL_W) / 2;  // cal starting of x pixel for value disp
+//  Serial.println(x_start_pixel);
+  if (x_start_pixel < 0 || x_start_pixel > MAX_W) {
+    x_start_pixel = 20;
+  }
+  return x_start_pixel;
+}
+
+void show_page(char value[], int page_type) {
+  char val[8];
+  u8g2.clearBuffer();                   // clear the internal memory
+  switch (page_type) {
+    case TEMP:
+      strcpy(val, value);
+      strcat(val, TEMP_c);
+      draw_page_contents("CPU Temp:", val, find_x_ofst(value, TEMP_c), 40);
+      break;
+    case RPM:
+      strcpy(val, value);
+      strcat(val, RPM_c);
+      draw_page_contents("CPU .RPM", val, find_x_ofst(value, RPM_c), 40);
+      break;
+    case GHZ:
+      strcpy(val, value);
+      strcat(val, GHZ_c);
+      draw_page_contents("CPU Freq", val, find_x_ofst(value, GHZ_c), 40);
+      break;
+    default:
+      break;
+  }
+  u8g2.sendBuffer();                    // transfer internal memory to the display
+}
+
+void draw_dot(int y) {
+  u8g2.clearBuffer();  
+  u8g2.drawBox(y, 40, 1, 1);
+  u8g2.drawBox(y*2, 40, 1, 1);
+  u8g2.drawBox(y*3, 40, 1, 1);
+  u8g2.sendBuffer();   
+}
+ 
+void loop(void) {
+  show_page("1400", RPM);
+  delay(3000);  
+  show_page("33", TEMP);
+  delay(3000);  
+  show_page("5.1", GHZ);
+  delay(3000);  
+  
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# desktop_internal_display
